d14cc7cd432f4da56efaa6d3b82233a41d8c9d2d
Jon Kirkman <jonkirkman@gmail.com>
2013-07-08 22:38:58 -0600

    added an == operator to Location class

--- lib/src/location.dart
+++ lib/src/location.dart
@@ -2,31 +2,33 @@
 part of modestmaps;
 
 class Location {
-	num lat = 0, lon = 0;
+  num lat = 0, lon = 0;
 
-	static final double deg2rad = PI / 180;
+  static final double deg2rad = PI / 180;
 
   Location(this.lat, this.lon);
 
-	String toString() => "(${lat.toStringAsFixed(3)}, ${lon.toStringAsFixed(3)})";
+  bool operator ==(other) => identical(lat, other.lat) && identical(lon, other.lon);
 
-	Location copy() => new Location(lat, lon);
+  String toString() => "(${lat.toStringAsFixed(3)}, ${lon.toStringAsFixed(3)})";
 
-	// returns approximate distance between start and end locations
-	//
-	// default unit is meters
-	//
-	// you can specify different units by optionally providing the
-	// earths radius in the units you desire
-	//
-	// Default is 6,378,000 metres, suggested values are:
-	//
-	// * 3963.1 statute miles
-	// * 3443.9 nautical miles
-	// * 6378 km
-	//
-	// see [Formula and code for calculating distance based on two lat/lon locations](http://jan.ucc.nau.edu/~cvm/latlon_formula.html)
-	static num distance(Location l1, Location l2, [num r = 6378000]) {
+  Location copy() => new Location(lat, lon);
+
+  // returns approximate distance between start and end locations
+  //
+  // default unit is meters
+  //
+  // you can specify different units by optionally providing the
+  // earths radius in the units you desire
+  //
+  // Default is 6,378,000 metres, suggested values are:
+  //
+  // * 3963.1 statute miles
+  // * 3443.9 nautical miles
+  // * 6378 km
+  //
+  // see [Formula and code for calculating distance based on two lat/lon locations](http://jan.ucc.nau.edu/~cvm/latlon_formula.html)
+  static num distance(Location l1, Location l2, [num r = 6378000]) {
     num a1 = l1.lat * deg2rad,
         b1 = l1.lon * deg2rad,
         a2 = l2.lat * deg2rad,
@@ -35,13 +37,13 @@ class Location {
          d = cos(a1) * sin(b1) * cos(a2) * sin(b2),
          e = sin(a1) * sin(a2);
     return acos(c + d + e) * r;
-	}
+  }
 
-	// Interpolates along a great circle, f between 0 and 1
-	//
-	// * FIXME: could be heavily optimized (lots of trig calls to cache)
-	// * FIXME: could be inmproved for calculating a full path
-	static Location interpolate(Location l1, Location l2, f) {
+  // Interpolates along a great circle, f between 0 and 1
+  //
+  // * FIXME: could be heavily optimized (lots of trig calls to cache)
+  // * FIXME: could be inmproved for calculating a full path
+  static Location interpolate(Location l1, Location l2, f) {
     if (l1 == l2) {
       return new Location(l1.lat, l1.lon);
     }
@@ -66,12 +68,12 @@ class Location {
     num lonN = atan2(y, x);
 
     return new Location(latN / deg2rad, lonN / deg2rad);
-	}
+  }
 
-	// Returns bearing from one point to another
-	//
-	// * FIXME: bearing is not constant along significant great circle arcs.
-	static double bearing(Location l1, Location l2) {
+  // Returns bearing from one point to another
+  //
+  // * FIXME: bearing is not constant along significant great circle arcs.
+  static double bearing(Location l1, Location l2) {
     final num lat1 = l1.lat * deg2rad,
               lon1 = l1.lon * deg2rad,
               lat2 = l2.lat * deg2rad,
@@ -84,5 +86,5 @@ class Location {
 
     // map it into 0-360 range
     return (result < 0) ? result + 360 : result;
-	}
+  }
 }
